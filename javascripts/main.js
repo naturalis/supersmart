4faf100342cfe0c354ff824c18d8a77259e62c67
Rutger Vos <rutgeraldo@gmail.com>
2014-10-03 01:31:34 +0200

    updated layout

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,16 @@
 var app = {
-  /* set up the UI behaviours */
-  initialize : function() {
+	/* set up the UI behaviours */
+	initialize : function() {
       
-      // add the handlers that scroll to the wanted section
-  }
+		// add the handlers that scroll to the wanted section
+		$('.scrollable').each(function(){
+			var href = this.hash;
+			$(this).on('click',function(){
+				$('html, body').animate({
+					scrollTop: $(href).offset().top
+				}, 500);
+				return false;
+			});      
+		});		
+	}
 }
