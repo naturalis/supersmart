11663f9eb474989320d1be858fbdeef9ec047a75
Rutger Vos <rutgeraldo@gmail.com>
2014-10-04 11:57:40 +0200

    various fixes

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,6 +11,22 @@ var app = {
 				}, 500);
 				return false;
 			});      
+		});
+		
+		// set the OS version for UI elements
+		var OSName="Unknown OS";
+		if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+		if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+		if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+		if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+		this.os = OSName;
+		
+		// set the terminal commands
+		$('.term').each(function(){
+			if ( OSName !== 'Windows' ) {
+				$(this).text('$');
+			}
 		});		
+				
 	}
 }
