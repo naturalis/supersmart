9dea9a01ff594a3fb8ad11d31366696039b6f662
hettling <hannes.hettling@gmail.com>
2016-06-16 12:24:08 +0200

    Revert "try travis build on trusty"
    This reverts commit 4d983499c3093bd01482665810404b3c3c4050a0.

--- conf/manifests/default.pp
+++ conf/manifests/default.pp
@@ -104,18 +104,18 @@ class install {
 	"exabayes_link":
 	  path    => "${tools_bin_dir}/exabayes",
 	  ensure  => link,
-	  target  => "${tools_dir}/exabayes-1.4.1/exabayes",
-	  require => Exec["compile_exabayes"];
+	  target  => "${tools_dir}/exabayes-1.4.1/bin/bin/exabayes",
+	  require => Exec["unzip_exabayes"];
 	"parse_exabayes_link":
 	  path  => "${tools_bin_dir}/parse-exabayes",
 	  ensure  => link,
-	  target  => "${tools_dir}/exabayes-1.4.1/parser",
-	  require => Exec["compile_exabayes"];
+	  target  => "${tools_dir}/exabayes-1.4.1/bin/bin/parser",
+	  require => Exec["unzip_exabayes"];
 	"exabayes_consense_link":
 	  path  => "${tools_bin_dir}/consense-exabayes",
 	  ensure  => link,
-	  target  => "${tools_dir}/exabayes-1.4.1/consense",
-	  require => Exec["compile_exabayes"];
+	  target  => "${tools_dir}/exabayes-1.4.1/bin/bin/consense",
+	  require => Exec["unzip_exabayes"];
 	"raxml_link":
 	  path    => "${tools_bin_dir}/raxml",
 	  ensure  => link,
@@ -250,16 +250,16 @@ class install {
 	  cwd     => $tools_dir,
 	  creates => "${tools_dir}/exabayes-1.4.1/",
 	  require => Exec[ "download_exabayes" ];
-	"configure_exabayes":		
-	  command => "sh configure --enable-mpi",		
-	  cwd     => "${tools_dir}/exabayes-1.4.1",		
-	  creates => "${tools_dir}/exabayes-1.4.1/exabayes/Makefile",		
-	  require => Exec[ "unzip_exabayes" ];
-	"compile_exabayes":		
-	  command => "make",		
-	  cwd     => "${tools_dir}/exabayes-1.4.1",		
-	  creates => "${tools_dir}/exabayes-1.4.1/exabayes",		
-	  require => Exec[ "configure_exabayes" ];
+#	"configure_exabayes":		
+#	  command => "sh configure --enable-mpi",		
+#	  cwd     => "${tools_dir}/exabayes-1.4.1",		
+#	  creates => "${tools_dir}/exabayes-1.4.1/exabayes/Makefile",		
+#	  require => Exec[ "unzip_exabayes" ];
+#	"compile_exabayes":		
+#	  command => "make",		
+#	  cwd     => "${tools_dir}/exabayes-1.4.1",		
+#	  creates => "${tools_dir}/exabayes-1.4.1/exabayes",		
+#	  require => Exec[ "configure_exabayes" ];
 
 	# install supersmart
 	"clone_supersmart":
